329c38a2487dc08ee1140fcd27f2c773e0f44807
mfrdr <francisca.d.rodrigues@gmail.com>
2024-02-25 00:20:23 +0100

    not working - plankton sinking tooooo fast

--- pformat.py
+++ pformat.py
@@ -1,3 +1,5 @@
+RESOLUTION = 0.01
+
 
 # Axis
 AX_TIME_D = 'Time [days]'
@@ -5,10 +7,11 @@ AX_DEPTH = 'Depth [m]'
 AX_NTR_LGT_PP = 'Normalized Concentration of Nutrients, Light Intensity and Phytoplankton'
 
 # Titles 
-T_NUTRIENTS = 'Nutrients Concentration Over Time and Depth'
-T_PLANKTON = 'Plankton Concentration Over Time and Depth'
-T_LIGHT = 'Light Intensity Over Time and Depth'
-T_GROWTH = 'Growth Rate Over Time and Depth'
+T_NUTRIENTS = 'Nutrients Concentration'
+T_PLANKTON = 'Plankton Concentration'
+T_DETRITUS = 'Detritus Concentration'
+T_LIGHT = 'Light Intensity'
+T_GROWTH = 'Growth Rate'
 T_NTR_LGT_PP = 'Nutrients, Phytoplankton and Light Intensity Over Depth'
 
 # Labels 
@@ -16,6 +19,7 @@ L_NUTRIENTS = 'Nutrients Concentration [umol/m^3]'
 L_NUTRIENTS_N = 'Nutrients Concentration'
 L_PLANKTON = 'Plankton Concentration [umol/m^3]'
 L_PLANKTON_N = 'Plankton Concentration'
+L_DETRITUS = 'Detritus Concentration [umol/m^3]'
 L_LIGHT = 'Light Intensity [umol photons /m^2 /day]'
 L_LIGHT_N = 'Light Intensity'
 L_GROWTH = 'Growth Rate [/day]'
